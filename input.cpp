0877029e056ba7c20106cff0fe7c4a1be60d189b
sushant054 <sushantnalage54@gmail.com>
2024-02-13 09:24:56 +0530

    taking input from user

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//taking input from user
+int main() {
+  int a;
+  cout << "Type a number: "; 
+  cin >> a; 
+  cout << "Your number is: " << a;
+  return 0;
+}
