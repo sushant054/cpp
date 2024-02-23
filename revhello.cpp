4b5f00d2431c163fa4c29e58c6b08ee021624e09
sushant054 <sushantnalage54@gmail.com>
2024-02-23 21:18:47 +0530

    printing string(reverse order)

---
+++ revhello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main() 
+{
+    std::string hello = "Hello, World!";
+    for (int i = hello.length() - 1; i >= 0; --i) {
+        std::cout << hello[i];
+    }
+    std::cout << std::endl;
+    return 0;
+}
+//output:-!dlroW ,olleH
