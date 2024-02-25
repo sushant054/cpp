6cf854890a0add1a824f559324deb512ff143784
sushant054 <sushantnalage54@gmail.com>
2024-02-25 15:02:50 +0530

    sum of two numbers

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Enter first number: ";
+  cin >> x;
+  cout << "Enter second number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0;
+}
