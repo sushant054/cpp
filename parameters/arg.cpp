917eeee87a5bbf8e5aa4a8e48f0fe38a43409c6b
sushant054 <sushantnalage54@gmail.com>
2024-03-01 21:43:16 +0530

    parameters/argument

---
+++ parameters/arg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string fname) 
+{
+  cout << fname << " sushant\n";
+}
+
+int main() {
+  myFunction("Hello");
+  return 0;
+}
