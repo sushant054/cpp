b19ea4063fa6f4f28b742821cda39deb1cc8f5de
sushant054 <sushantnalage54@gmail.com>
2024-03-11 11:36:39 +0530

    copy string object..

--- revno.cpp
+++ revno.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main() 
+{
 
   int n, reversed_number = 0, remainder;
 
@@ -15,4 +16,5 @@ int main() {
 
   cout << "Reversed Number = " << reversed_number;
   return 0;
+
 }
