bce975291593b87f1c73763f2b325393cee30de8
sushant054 <sushantnalage54@gmail.com>
2024-03-16 18:42:03 +0530

    string char length..

---
+++ length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+string str;
+cin>>str;
+int count = 0;
+for(int i = 0;str[i];i++) // till the string character is null
+    count++;
+cout<<count;
+}
+//eg. Input: abcde
+// Output: 5
