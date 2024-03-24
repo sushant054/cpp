e143ed65b15fb83e61f9ecf12c6a70da6a11c368
sushant054 <sushantnalage54@gmail.com>
2024-03-24 15:54:17 +0530

    example of multi-dimensional array

---
+++ multidimarray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string letters[2][4] = {
+    { "V", "I", "D", "N" },
+    { "Y", "A", "N", "I" }
+  };
+  cout<<"fav.char::"<<letters[0][0];
+  return 0;
+}
