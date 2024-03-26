2d2dc1d1a169a41b78c6fabc23d6b30f204d81bb
sushant054 <sushantnalage54@gmail.com>
2024-03-26 18:24:08 +0530

    constructor

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class MyClass 
+{    
+  public:           // Access specifier
+    MyClass() {     // Constructor
+      cout << "Hello World!!";
+      
+    }
+};
+
+int main() 
+{
+  MyClass myObj;    // Create an object of MyClass (this will call the constructor)
+  return 0;
+}
