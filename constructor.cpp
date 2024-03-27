b0e4c265f99407e1d8363bc274f5670c6f23cd47
sushant054 <sushantnalage54@gmail.com>
2024-03-27 22:32:20 +0530

    printing various datatypes

--- constructor.cpp
+++ constructor.cpp
@@ -5,7 +5,7 @@ class MyClass
   public:           // Access specifier
     MyClass() {     // Constructor
       cout << "Hello World!!";
-      
+
     }
 };
 
@@ -14,3 +14,4 @@ int main()
   MyClass myObj;    // Create an object of MyClass (this will call the constructor)
   return 0;
 }
+
---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//printing various datatypes...
+int main()
+{
+	cout << "Size of char : " << sizeof(char) << endl;
+	cout << "Size of int : " << sizeof(int) << endl;
+
+	cout << "Size of long : " << sizeof(long) << endl;
+	cout << "Size of float : " << sizeof(float) << endl;
+
+	cout << "Size of double : " << sizeof(double) << endl;
+
+	return 0;
+}
