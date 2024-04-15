24bf230deddffc3f7627ba2e1d5f64b583099647
sushant054 <sushantnalage54@gmail.com>
2024-04-15 09:53:49 +0530

    class methods...

---
+++ method.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {         // The class
+  public:               // Access specifier
+    void myMethod() {   // Method/function
+      cout << "Hello World!";
+    }
+};
+
+int main() {
+  MyClass myObj;     // Create an object of MyClass
+  myObj.myMethod();  
+  
+  return 0;
+}
