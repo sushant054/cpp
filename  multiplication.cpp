eb1ee5d5204718544161792325d4df12c16bf8e2
sushant054 <sushantnalage54@gmail.com>
2024-04-22 12:09:30 +0530

    multiplication table...

---  multiplication.cpp
+++  multiplication.cpp
@@ -10,9 +10,12 @@ int main() {
 
     // run a loop from 1 to 10
     // print the multiplication table
-    for (int i = 1; i <= 10; ++i) {
+    for (int i = 1; i <= 10; ++i) 
+    {
         cout << n << " * " << i << " = " << n * i << endl;
+
     }
-    
+
     return 0;
+
 }
