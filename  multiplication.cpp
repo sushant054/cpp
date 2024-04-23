359b84a689f3fbf55a18bec21c454cdb41612bd0
sushant054 <sushantnalage54@gmail.com>
2024-04-23 23:31:32 +0530

    Multiplication table...

---  multiplication.cpp
+++  multiplication.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 int main() {
-
     int n;
-
     cout << "Enter a positive integer: ";
     cin >> n;
 
     // run a loop from 1 to 10
+    
     // print the multiplication table
     for (int i = 1; i <= 10; ++i) 
     {
