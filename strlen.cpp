9039a7ff07f4fea1ce113f97a05cbafdad2b6c2b
sushant054 <sushantnalage54@gmail.com>
2024-05-11 11:08:11 +0530

    facotrial...

--- strlen.cpp
+++ strlen.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     string str = "C++ Programming";
 
-    // you can also use str.length()
+    // you can also use str.length() function
     cout << "String Length = " << str.size();
 
     return 0;
