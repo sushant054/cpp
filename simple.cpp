2a19c9af63ad4d06bf5830afea94e92028c0f2dc
sushant054 <sushantnalage54@gmail.com>
2024-05-18 22:05:52 +0530

    simple calculator..

--- simple.cpp
+++ simple.cpp
@@ -31,7 +31,6 @@ int main() {
       break;
 
     default:
-      // If the operator is other than +, -, * or /, error message is shown
       cout << "Error! operator is not correct";
       break;
   }
