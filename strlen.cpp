19522a93d4cf02bde8d56fea2535e3c5904b85c9
sushant054 <sushantnalage54@gmail.com>
2024-05-22 22:37:45 +0530

    string length..

--- strlen.cpp
+++ strlen.cpp
@@ -5,7 +5,6 @@ int main() {
     string str = "C++ Programming";
 
     // you can also use str.length() function 
-    //shows string length...
     cout << "String Length = " << str.size();
 
     return 0;
