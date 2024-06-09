9a10eb896712e33f90827fb4797ab6bbea798409
sushant054 <sushantnalage54@gmail.com>
2024-06-09 21:37:49 +0530

    Enumeration...

--- enum.cpp
+++ enum.cpp
@@ -6,7 +6,8 @@ enum Level {
   MEDIUM,
   HIGH
 }; 
-
+//Note that if you assign a value to one specific item, the next items will update their numbers accordingly:
+//eg.LOW=5, then MEDIUM=6, and high =7..
 int main() {
   enum Level myVar =HIGH;
   cout << myVar;
